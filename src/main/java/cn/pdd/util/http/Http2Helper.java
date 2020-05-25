b528ccdebb4c9b35e156b607c554d06cf7e9fe09
pdd <pdd@pdd-pc>
2020-05-25 11:53:13 +0800

    添加okhttp3带cookie

--- src/main/java/cn/pdd/util/http/Http2Helper.java
+++ src/main/java/cn/pdd/util/http/Http2Helper.java
@@ -1,14 +1,19 @@
 /**
- * 
+ *
  */
 package cn.pdd.util.http;
 
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.log4j.Logger;
 
 import okhttp3.ConnectionPool;
+import okhttp3.Cookie;
+import okhttp3.CookieJar;
 import okhttp3.Dispatcher;
 import okhttp3.FormBody;
 import okhttp3.HttpUrl;
@@ -25,7 +30,7 @@ import okhttp3.logging.HttpLoggingInterceptor;
  * @author paddingdun
  * 基于okhttp3实现的工具类;
  * 注意:该类对于参数的默认编码就是:"utf-8"
- * 
+ *
  * 该类每次使用都应该重新创建Http2Helper对象(这样就可以多线程使用);
  * 切记一个对象多次调用
  * 2018年7月3日
@@ -34,23 +39,23 @@ import okhttp3.logging.HttpLoggingInterceptor;
  */
 @SuppressWarnings({"unused"})
 public class Http2Helper {
-	
+
 	/**
 	 * Http2Helper 日志变量;
 	 */
 	private final static Logger logger = Logger.getLogger(Http2Helper.class);
-	
+
 	private final static int DEFAULT_TIMEOUT = -1;
 
 	private static OkHttpClient client = null;
-	
+
 	static {
 		ConnectionPool connectionPool = new ConnectionPool(5, 1, TimeUnit.MINUTES);
 		//dispatcher 用来控制总请求次数;
 		Dispatcher dispatcher = new Dispatcher();
 		dispatcher.setMaxRequests(64);
 		dispatcher.setMaxRequestsPerHost(4);
-		
+
 		HttpLoggingInterceptor interceptor =new HttpLoggingInterceptor(new HttpLoggingInterceptor.Logger() {
 			@Override
 			public void log(String message) {
@@ -60,7 +65,8 @@ public class Http2Helper {
 			}
 		});
 		interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
-		
+
+//		final HashMap<String, List<Cookie>> cookieStore = new HashMap<>();
 		client = new OkHttpClient.Builder()
 				.connectionPool(connectionPool)
 				.dispatcher(dispatcher)
@@ -68,27 +74,41 @@ public class Http2Helper {
 				.connectTimeout(10, TimeUnit.SECONDS)//连接超时时间
 		        .readTimeout(10, TimeUnit.SECONDS)//读的时间
 		        .writeTimeout(10, TimeUnit.SECONDS)//写的时间
+//		        .cookieJar(new CookieJar() {
+//			        @Override
+//			        public void saveFromResponse(HttpUrl httpUrl, List<Cookie> list) {
+//			            cookieStore.put(httpUrl.host(), list);
+//			        }
+//
+//			        @Override
+//			        public List<Cookie> loadForRequest(HttpUrl httpUrl) {
+//			            List<Cookie> cookies = cookieStore.get(httpUrl.host());
+//			            return cookies != null ? cookies : new ArrayList<Cookie>();
+//			        }
+//			    })
 				.build();
+
+
 	}
-	
+
 	private Request request = null;
 	private Request.Builder requestBuilder = null;
-	
+
 	private RequestBody requestBody = null;
 	private FormBody.Builder formBodyBuilder = null;
 
-	
-	
+
+
 	private int type;
 	private String url;
-	
+
 	private Http2Helper(String url, int type, Charset charset){
 		this.url = url;
 		this.type = type;
 		this.requestBuilder = new Request.Builder();
 		this.formBodyBuilder = new FormBody.Builder(charset);
 	}
-	
+
 	/**
 	 * 参数默认将是utf-8编码
 	 * @param url
@@ -97,30 +117,30 @@ public class Http2Helper {
 	public static Http2Helper post(String url) {
         return new Http2Helper(url, 1, Util.UTF_8);
     }
-	
+
 	public static Http2Helper post(String url, Charset charset) {
         return new Http2Helper(url, 1, charset);
     }
-	
+
 	public static Http2Helper get(String url) {
         return new Http2Helper(url, 2, Util.UTF_8);
     }
-	
+
 	public Http2Helper setContentType(final String mimeType) {
 		this.requestBuilder.header("Content-Type", mimeType);
         return this;
     }
-	
+
 	public Http2Helper addHeader(final String name, final String value) {
 		this.requestBuilder.addHeader(name, value);
         return this;
     }
-	
+
 	public Http2Helper setHeader(final String name, final String value) {
 		this.requestBuilder.header(name, value);
         return this;
     }
-	
+
 	/**
 	 * 参数和值未经过urlencode编码;
 	 * @param name
@@ -131,7 +151,7 @@ public class Http2Helper {
 		formBodyBuilder.add(name, value);
         return this;
     }
-	
+
 	/**
 	 * 参数和值已经过urlencode编码;
 	 * @param name
@@ -142,7 +162,7 @@ public class Http2Helper {
 		formBodyBuilder.addEncoded(name, value);
         return this;
     }
-	
+
 	/**
 	 * utf-8转成字节流;
 	 * @param json
@@ -152,7 +172,7 @@ public class Http2Helper {
 		this.requestBody = RequestBody.create(null, json);
         return this;
     }
-	
+
 	/**
 	 * 以指定的编码方式转成字节流;
 	 * @param json
@@ -164,15 +184,15 @@ public class Http2Helper {
 		this.requestBody = RequestBody.create(MediaType.parse("application/json; charset=" + c_name), json);
         return this;
     }
-	
+
 	public String executeToString()throws Exception {
 		return executeToString(DEFAULT_TIMEOUT, DEFAULT_TIMEOUT, DEFAULT_TIMEOUT, null, true);
 	}
-	
+
 	public String executeToString(Charset charset)throws Exception {
 		return executeToString(DEFAULT_TIMEOUT, DEFAULT_TIMEOUT, DEFAULT_TIMEOUT, charset, true);
 	}
-	
+
 	public String executeToString(int connectTimeout, int readTimeout, int writeTimeout, Charset charset, boolean followRedirects)throws Exception {
 		Response response = null;
 		try {
@@ -192,7 +212,7 @@ public class Http2Helper {
 			}
 		}
 	}
-	
+
 	/**
 	 * 单位:秒;
 	 * @param connectTimeout
@@ -224,8 +244,8 @@ public class Http2Helper {
 			}
 			this.request = this.requestBuilder.url(hu).get().build();
 		}
-		
-		
+
+
 		if(this.request != null) {
 			OkHttpClient c = null;
 			if(connectTimeout == DEFAULT_TIMEOUT
@@ -237,7 +257,7 @@ public class Http2Helper {
 				Util.checkDuration("connectTimeout", connectTimeout, TimeUnit.SECONDS);
 				Util.checkDuration("readTimeout", readTimeout, TimeUnit.SECONDS);
 				Util.checkDuration("writeTimeout", writeTimeout, TimeUnit.SECONDS);
-				
+
 				c = client.newBuilder()
 				.followRedirects(followRedirects)
 				.connectTimeout(connectTimeout, TimeUnit.SECONDS)//连接超时时间
@@ -252,11 +272,11 @@ public class Http2Helper {
 			throw new RuntimeException("无效的请求类型!");
 		}
 	}
-	
+
 	public Response execute()throws Exception {
 		return execute(DEFAULT_TIMEOUT, DEFAULT_TIMEOUT, DEFAULT_TIMEOUT, true);
 	}
-	
+
 	public static void destroy() {
 		if(null != client) {
 			try {
@@ -267,7 +287,7 @@ public class Http2Helper {
 			}
 		}
 	}
-	
+
 	public static void main(String[] args) throws Exception{
 		Http2Helper helper = Http2Helper.get("http://www.baidu55.com");
 		String s = helper.executeToString(5,1,1, null, false);
